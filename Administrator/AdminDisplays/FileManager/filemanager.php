bfb0a7ab141350260980252ff8d0a54a5d344373
MAKIENAUT <mcrayescoto@gmail.com>
2023-04-10 11:34:05 +0800

    PUSHED

--- Administrator/AdminDisplays/FileManager/filemanager.php
+++ Administrator/AdminDisplays/FileManager/filemanager.php
@@ -18,22 +18,22 @@ $folders = array_filter(glob($dir . '*'), 'is_dir');
 $selected_folder = isset($_GET['folder']) ? htmlspecialchars($_GET['folder']) : null;
 
 if ($selected_folder) {
-	$files = array_filter(glob($dir . $selected_folder . '/*.{pdf,doc,docx,txt}', GLOB_BRACE), 'is_file');
+   $files = array_filter(glob($dir . $selected_folder . '/*.{pdf,doc,docx,txt}', GLOB_BRACE), 'is_file');
 }
 
 function downloadFile($file)
 {
-	if (file_exists($file)) {
-		header('Content-Description: File Transfer');
-		header('Content-Type: application/octet-stream');
-		header('Content-Disposition: attachment; filename="' . basename($file) . '"');
-		header('Expires: 0');
-		header('Cache-Control: must-revalidate');
-		header('Pragma: public');
-		header('Content-Length: ' . filesize($file));
-		readfile($file);
-		exit;
-	}
+   if (file_exists($file)) {
+      header('Content-Description: File Transfer');
+      header('Content-Type: application/octet-stream');
+      header('Content-Disposition: attachment; filename="' . basename($file) . '"');
+      header('Expires: 0');
+      header('Cache-Control: must-revalidate');
+      header('Pragma: public');
+      header('Content-Length: ' . filesize($file));
+      readfile($file);
+      exit;
+   }
 }
 
 ?>
@@ -53,7 +53,7 @@ function downloadFile($file)
 </head>
 
 <body onload="nav_link(3)">
-<nav>
+   <nav>
       <header>
          <div class="header_image">
             <a href="../../../Pages/Home/home.html">
@@ -77,9 +77,8 @@ function downloadFile($file)
                <div class="dropdown">
                   <i class="fa-solid fa-gear"></i>
                   <div class="dropdown-content">
-                     <a href="../../AdminCommands/logout.php" 
-                        onclick="return confirm('Are you sure you want to logout?');"
-                     >
+                     <a href="../../AdminCommands/logout.php"
+                        onclick="return confirm('Are you sure you want to logout?');">
                         Logout
                      </a>
                      <a href="../AdminProfile/profile.php">Profile</a>
@@ -112,38 +111,49 @@ function downloadFile($file)
          </div>
       </div>
    </nav>
-   
-	<main>
-		<h1>File Management Page</h1>
-		<ul>
-			<?php foreach ($folders as $folder): ?>
-				<li>
-					<a href="?folder=<?php echo basename($folder); ?>">
-						<?php echo basename($folder); ?>
-					</a>
-				</li>
-			<?php endforeach; ?>
-		</ul>
-		<?php if ($selected_folder): ?>
-			<h2>Files in
-				<?php echo $selected_folder; ?>
-			</h2>
-			<?php if (count($files)): ?>
-				<ul class="grid-container">
-					<?php foreach ($files as $file): ?>
-						<li class="grid-item">
-							<i class="icon fa fa-folder"></i>
-							<a href="<?php echo $file; ?>" download>
-								<?php echo basename($file); ?>
-							</a>
-						</li>
-					<?php endforeach; ?>
-				</ul>
-			<?php else: ?>
-				<p>No files found.</p>
-			<?php endif; ?>
-		<?php endif; ?>
-	</main>
+
+   <main>
+      <div class="main_title">
+         <h1>File Management Page</h1>
+      </div>
+      <div class="manager_display">
+         <div class="applicant_folder">
+            <?php foreach ($folders as $folder): ?>
+               <div class="folder_link">
+                  <a href="?folder=<?php echo basename($folder); ?>"
+                     class="<?php echo ($selected_folder && $selected_folder === basename($folder)) ? 'active' : ''; ?>">
+                     <i class="fa-regular fa-folder "></i>
+                     <p class="<?php echo ($selected_folder && $selected_folder === basename($folder)) ? 'active' : ''; ?>">
+                        <?php echo basename($folder); ?>
+                     </p>
+                  </a>
+               </div>
+            <?php endforeach; ?>
+         </div>
+         <div class="selected_folder">
+            <?php if ($selected_folder): ?>
+               <div class="folder_title">
+                  <h2>Files in <?php echo $selected_folder; ?>
+                  </h2>
+               </div>
+               <?php if (count($files)): ?>
+               <div class="folder_container">
+                  <?php foreach ($files as $file): ?>
+                     <div class="file_display">
+                        <i class="icon fa fa-folder" id="folder_icon"></i>
+                        <a class="file_name" href="<?php echo $file; ?>" download><?php echo basename($file); ?></a>
+                     </div>
+                  <?php endforeach; ?>
+               </div>
+               <?php else: ?>
+                  <p>No files found.</p>
+               <?php endif; ?>
+            <?php endif; ?>
+         </div>
+      </div>
+
+   </main>
+
 
 </body>
 
