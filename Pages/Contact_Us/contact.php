798ccab24fe4e677661ffa43e35d24c0b329a82a
MAKIENAUT <mcrayescoto@gmail.com>
2023-04-21 09:24:22 +0800

    PUSHED: new changes

--- Pages/Contact_Us/contact.php
+++ Pages/Contact_Us/contact.php
@@ -53,38 +53,47 @@ $conn->close();
 </head>
 
 <body>
-   <!-- NAVIGATION BAR SECTION [ START ] -->
    <nav class="navbar" id="myNavBar">
-      <!-- LOGO SECTION [ START ] -->
+
       <div class="logo_holder">
          <a class="nav_logo" href="https://westmigrationagency.com/">
             <img src="../../Photos/wma-logo.png" />
          </a>
       </div>
-      <!-- LOGO SECTION [ START ] -->
-      <!-- LINKS SECTION [ START ] -->
+
       <div class="link_holder" id="link_holder">
          <div class="nav_links">
-            <a href="../Forms/interest_form.php">Forms</a>
+            <a href="#">News</a>
          </div>
-         <div class="nav_links">
-            <a href="../Process/process.html">Process</a>
+
+         <div class="nav_links dropdown">
+            <button class="dropbtn">Process</button>
+            <div class="dropdown-content">
+               <a href="../Process/process_FamBased.html">Family Based</a>
+               <a href="../Process/process_StudEx.html">Study and Exchange</a>
+               <a href="../Process/process_TempEmp.html">Temporary Employment</a>
+            </div>
          </div>
+
+         <div class="nav_links dropdown">
+            <button class="dropbtn">Categories</button>
+            <div class="dropdown-content">
+               <a href="../Forms/Categories/Family_Based/FB_Eligibility.php">Family Based</a>
+               <a href="../Forms/Categories/Study_Exchange/SE_Eligibility.php">Study and Exchange</a>
+               <a href="../Forms/Categories/Temporary_Employment/TE_Eligibility.php">Temporary Employment</a>
+               <a href="../Forms/Categories/">Other Uploads</a>
+            </div>
+         </div>
+
          <div class="nav_links">
             <a href="../About_Us/about.html">About Us</a>
          </div>
-         <div class="nav_links">
-            <a href="../Contact_Us/contact.php">Contact Us</a>
-         </div>
       </div>
-      <!-- LINKS SECTION [ END ] -->
-      <!-- BURGER MENU SECTION [ START ] -->
+
       <a href="javascript:void(0);" class="icon" onclick="myFunction()">
          <i class="fa fa-bars"></i>
       </a>
-      <!-- BURGER MENU SECTION [ START ] -->
    </nav>
-   <!-- NAVIGATION BAR SECTION [ END ] -->
 
    <!-- ------------------------------------ -->
 
@@ -107,38 +116,11 @@ $conn->close();
                <div class="form">
                   <h1>FOR INQUIRIES, PLEASE FILL UP FORM BELOW.</h1>
                   <form method="post">
-                     <input 
-                        required
-                        type="text" 
-                        id="lastname" 
-                        name="lastname" 
-                        placeholder="Last Name:"
-                     />
-                     <input 
-                        required
-                        type="text" 
-                        id="firstname" 
-                        name="firstname" 
-                        placeholder="Last Name:"
-                     />
-                     <input 
-                        required
-                        type="text" 
-                        id="phone" 
-                        name="phone" 
-                        placeholder="Last Name:"
-                     />
-                     <input 
-                        required
-                        type="email" 
-                        id="email" 
-                        name="email" 
-                        placeholder="Last Name:"
-                     />
-                     <textarea 
-                        id="message" 
-                        name="message"
-                        placeholder="Message">
+                     <input required type="text" id="lastname" name="lastname" placeholder="Last Name:" />
+                     <input required type="text" id="firstname" name="firstname" placeholder="Last Name:" />
+                     <input required type="text" id="phone" name="phone" placeholder="Last Name:" />
+                     <input required type="email" id="email" name="email" placeholder="Last Name:" />
+                     <textarea id="message" name="message" placeholder="Message">
                      </textarea>
                      <input class="submit" type="submit" value="Submit" style="cursor: pointer;">
                   </form>
