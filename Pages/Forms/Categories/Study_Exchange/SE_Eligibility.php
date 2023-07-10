4965fee7e452f0ee4eb2d03676bcbc140ae316e7
MAKIENAUT <mcrayescoto@gmail.com>
2023-07-10 21:53:49 +0800

    PUSHED

--- Pages/Forms/Categories/Study_Exchange/SE_Eligibility.php
+++ Pages/Forms/Categories/Study_Exchange/SE_Eligibility.php
@@ -57,8 +57,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submit'])) {
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
       integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
       crossorigin="anonymous" referrerpolicy="no-referrer" />
-   <link rel="stylesheet" href="SE_Eligibility.css">
-   <script src="SE_Eligibility.js"></script>
+   <link rel="stylesheet" href="se_eligibility.css">
+   <script src="se_eligibility.js"></script>
    <link rel="icon" type="image/x-icon" href="../../Photos/WMA.png">
    <title>Interest Form</title>
 </head>
